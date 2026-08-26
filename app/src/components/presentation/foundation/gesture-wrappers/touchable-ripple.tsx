f74f3f6edf8293c037bc12959e140cf58e7ff29b
gabsvm <53277787+gabsvm@users.noreply.github.com>
2026-08-26 13:58:34 -0300

    perf: use native Paper ripple press handling

--- app/src/components/presentation/foundation/gesture-wrappers/touchable-ripple.tsx
+++ app/src/components/presentation/foundation/gesture-wrappers/touchable-ripple.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable no-restricted-imports */
 import { GesturePressableProps } from '@/components/presentation/foundation/gesture-wrappers/pressable-props';
-import { isNotNullOrUndefined } from '@/utils/null';
-import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import {
   TouchableRippleProps,
   TouchableRipple as NativeTouchableRipple,
@@ -13,26 +11,12 @@ export default function TouchableRipple({
   disabled,
   ...rest
 }: GesturePressableProps<TouchableRippleProps>) {
-  const tap = Gesture.Tap()
-    .runOnJS(true)
-    .onStart(() => !disabled && onPress?.());
-  const longPress = onLongPress
-    ? Gesture.LongPress()
-        .runOnJS(true)
-        .onStart(() => !disabled && onLongPress())
-    : undefined;
-  const gesture = Gesture.Race(
-    ...[tap, longPress].filter(isNotNullOrUndefined),
-  );
   return (
-    <GestureDetector gesture={gesture}>
-      <NativeTouchableRipple
-        disabled={disabled}
-        onPress={onPress ? () => {} : undefined!}
-        // Disable long press since we should be using Holdable for this
-        onLongPress={onLongPress || onPress ? () => {} : undefined!}
-        {...rest}
-      />
-    </GestureDetector>
+    <NativeTouchableRipple
+      disabled={disabled}
+      onPress={onPress}
+      onLongPress={onLongPress}
+      {...rest}
+    />
   );
 }
