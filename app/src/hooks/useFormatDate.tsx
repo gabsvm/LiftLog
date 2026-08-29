bfa523c0b98211d5b30dff65a2de516b7271f2fe
gabsvm <53277787+gabsvm@users.noreply.github.com>
2026-08-29 03:27:45 -0300

    perf(locale): stabilize date formatter callback

--- app/src/hooks/useFormatDate.tsx
+++ app/src/hooks/useFormatDate.tsx
@@ -1,15 +1,19 @@
 import { LocalDate } from '@js-joda/core';
 import { useAppSelector } from '@/store';
+import { useCallback } from 'react';
 
 export function useFormatDate(): (
   date: LocalDate,
   opts: Intl.DateTimeFormatOptions,
 ) => string {
   const locale = useAppSelector((x) => x.settings.preferredLanguage);
-  return (date, opts) =>
-    new Date(
-      date.year(),
-      date.month().ordinal(),
-      date.dayOfMonth(),
-    ).toLocaleString(locale, opts);
+  return useCallback(
+    (date: LocalDate, opts: Intl.DateTimeFormatOptions) =>
+      new Date(
+        date.year(),
+        date.month().ordinal(),
+        date.dayOfMonth(),
+      ).toLocaleString(locale, opts),
+    [locale],
+  );
 }
